ba4563102e4302bd3eaab4061fdfaf63ddbe4a8d
beomwon <38881094+beomwon@users.noreply.github.com>
2023-01-21 11:35:20 +0900

    Delete recommend directory

--- recommend/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path, include
-
-urlpatterns = [
-    path('saveteamandmenu/', views.saveTeamAndMenu, name='saveteamandmenu'), 
-    path('teamlist/', views.teamList, name='teamlist'),
-    path('todaycafeteria/', views.todayCafeteria, name='todaycafeteria')
-]
