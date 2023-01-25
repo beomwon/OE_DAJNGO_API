d99b02bd3e27e59279f498324223b17bc7b2c408
beomwon <38881094+beomwon@users.noreply.github.com>
2023-01-25 10:13:07 +0900

    Delete osyulraeng directory

--- osyulraeng/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
-    path('store/', include('store.urls')),
-    path('recommend/', include('recommend.urls'))
-]
